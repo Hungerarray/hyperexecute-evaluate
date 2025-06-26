b8d990b0247a97b07051e828df98cf84ea648326
hungerarray <ranjan.ojha01@gmail.com>
2025-06-26 13:09:23 +0545

    reintroduce failing test cases

--- test_main.py
+++ test_main.py
@@ -3,34 +3,44 @@ import time
 
 # 5 test cases with different fixed durations
 
+
 def test_case_1():
     time.sleep(2)
     assert True
+
 
 def test_case_2():
     time.sleep(2)
     assert True
 
+
 def test_case_3():
     time.sleep(2)
     assert True
 
+
 def test_case_4():
-    time.sleep(120)
+    time.sleep(2)
     assert True
+
 
 def test_case_5():
     time.sleep(2)
     assert True
 
+
 def test_case_6():
-    time.sleep(120)
+    time.sleep(2)
     assert True
+
 
 # 2 test cases that always fail
 
-# def test_always_fail_1():
-#     assert False, "This test always fails."
 
-# def test_always_fail_2():
-#     pytest.fail("This test always fails.") 
+def test_always_fail_1():
+    assert False, "This test always fails."
+
+
+def test_always_fail_2():
+    pytest.fail("This test always fails.")
+
